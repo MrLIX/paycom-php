ec34400170a9a7f9f06092230e67be823c7df49b
nazirovlix <nazirovlix@mail.ru>
2018-06-02 14:20:43 +0500

    init files

---
+++ paycom.config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'merchant_id' => '<id>',
+    'login' => 'Paycom',
+    'keyFile' => 'password.paycom',
+    'db' => [
+        'database' => '<database>',
+        'username' => '<username>',
+        'password' => '<password>'
+    ],
+];
